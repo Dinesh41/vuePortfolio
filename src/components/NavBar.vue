7437c06694bac9f68c203a4473225512ff1566d0
Dinesh S <dinesh.s@ictgroup.ind.in>
2020-02-11 13:28:08 +0530

    Highlight on scroll added

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -12,6 +12,9 @@
   </div>
 </template>
 <script>
+import jQuery from "jquery";
+const $ = jQuery;
+window.$ = $;
 export default {
   props: {
     nav_items: Array,
@@ -23,23 +26,67 @@ export default {
     };
   },
   mounted() {
-    this.markActiveItemInNavBar();
-    window.onscroll = function() {
-      console.log(this.scrollTop());
-    };
-    window.onhashchange = () => {
-      this.markActiveItemInNavBar();
-    };
+    this.enableHighlightOnScroll("sdf");
   },
   methods: {
-    markActiveItemInNavBar() {
-      let routeStartIndex = window.location.href.indexOf("#");
-      if (routeStartIndex == -1) {
-        this.currentActiveRoute = this.default_nav_link;
-        return;
-      }
-      let currentRoute = window.location.href.substring(routeStartIndex);
-      this.currentActiveRoute = currentRoute;
+    enableHighlightOnScroll(listId) {
+      listId;
+      // Cache selectors
+      var lastId,
+        topMenu = $("#nav_menu"),
+        topMenuHeight = topMenu.outerHeight() + 1,
+        // All list items
+        menuItems = topMenu.find("a"),
+        // Anchors corresponding to menu items
+        scrollItems = menuItems.map(function() {
+          var item = $($(this).attr("href"));
+          if (item.length) {
+            return item;
+          }
+        });
+
+      // Bind click handler to menu items
+      // so we can get a fancy scroll animation
+      menuItems.click(function(e) {
+        var href = $(this).attr("href"),
+          offsetTop =
+            href === "#" ? 0 : $(href).offset().top - topMenuHeight + 1;
+        $("html, body")
+          .stop()
+          .animate(
+            {
+              scrollTop: offsetTop
+            },
+            850
+          );
+        e.preventDefault();
+      });
+
+      // Bind to scroll
+      $(window).scroll(function() {
+        // Get container scroll position
+        var fromTop = $(this).scrollTop() + topMenuHeight;
+
+        // Get id of current scroll item
+        var cur = scrollItems.map(function() {
+          if ($(this).offset().top < fromTop) return this;
+        });
+        // Get the id of the current element
+        cur = cur[cur.length - 1];
+        var id = cur && cur.length ? cur[0].id : "";
+
+        if (lastId !== id) {
+          lastId = id;
+          // Set/remove active class
+          menuItems
+            .parent()
+            .removeClass("isActive")
+            .end();
+          $('a[href$="' + id + '"')
+            .parent()
+            .addClass("isActive");
+        }
+      });
     }
   }
 };
